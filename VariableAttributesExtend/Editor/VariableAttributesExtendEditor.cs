f959c62c2e9d53b8572f79995ffa210334fd8dc9
YuloongBY <boyang.yaya@gmail.com>
2022-02-23 10:38:35 +0900

    Update

---
+++ VariableAttributesExtend/Editor/VariableAttributesExtendEditor.cs
@@ -0,0 +1,377 @@
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(UnityEngine.Object), true, isFallback = true)]
+[CanEditMultipleObjects]
+public class VariableAttributesExtendEditor : Editor
+{
+	private Dictionary<Type, Attributes> attributesArray_ = new Dictionary<Type, Attributes>();
+	private List<FieldInfo> objectFields_;
+	private bool initialized_;
+	private SerializedProperty mSprictProp_;
+
+	private GUIStyle scriptTagStyle_;
+	private GUIStyle categorryTitleStyle_;
+
+	private void Awake()
+	{
+        {
+			scriptTagStyle_ = new GUIStyle(EditorStyles.label);
+			scriptTagStyle_.fontSize = 9;
+			scriptTagStyle_.normal.textColor = EditorGUIUtility.isProSkin ? new Color(0.4f,0.4f,0.4f) : new Color(0.2f, 0.2f, 0.2f);
+		}
+		{
+			categorryTitleStyle_ = new GUIStyle(EditorStyles.label);
+			categorryTitleStyle_.fontSize = 13;
+			categorryTitleStyle_.normal.textColor = EditorGUIUtility.isProSkin ? new Color(0.6f,0.6f,0.6f) : new Color(0.38f, 0.38f, 0.38f);
+		}
+	}
+
+	void OnEnable()
+	{
+		// 获取类型和可序列化属性
+		Type type = target.GetType();
+		List<FieldInfo> fields = new List<FieldInfo>();
+		FieldInfo[] array = type.GetFields(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.DeclaredOnly);
+		fields.AddRange(array);
+
+		var types = new List<Type>();
+		// 获取父类的可序列化属性
+		while (IsTypeCompatible(type.BaseType) && type != type.BaseType)
+		{
+			types.Add( type );
+			type = type.BaseType;
+			array = type.GetFields(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.DeclaredOnly);
+			fields.InsertRange(0, array);
+		}
+
+		if( fields.Count > 0 )
+        {
+			//删除非序列化内容
+			for (int nCnt = fields.Count - 1; nCnt >= 0; nCnt--)
+			{
+				// 非公有并且没有添加[SerializeField]特性的属性
+				if (!fields[nCnt].IsPublic)
+				{
+					object[] serials = fields[nCnt].GetCustomAttributes(typeof(SerializeField), true);
+					if (serials.Length == 0)
+					{
+						fields.Remove(fields[nCnt]);						
+					}
+				}
+				else
+				{
+					//公有但是添加了[HideInInspector]特性的属性
+					object[] hides = fields[nCnt].GetCustomAttributes(typeof(HideInInspector), true);
+					if (hides.Length != 0)
+					{
+						fields.Remove(fields[nCnt]);						
+					}
+				}
+			}
+
+			//排列
+			objectFields_ = fields;
+
+			//AttributesArray初期化
+			foreach (var field in objectFields_)
+			{
+				if (!attributesArray_.ContainsKey(field.DeclaringType))
+				{
+					attributesArray_.Add(field.DeclaringType, new Attributes());
+				}
+			}
+			
+			Repaint();
+			initialized_ = false;
+		} 
+	}
+
+	private void OnDisable()
+	{
+		//解放
+		if( attributesArray_ != null )
+        {
+			foreach (var attribute in attributesArray_)
+			{
+				attribute.Value.AllClear();
+			}
+			attributesArray_.Clear();
+		}
+	}
+
+	public override void OnInspectorGUI()
+	{
+		EditorGUI.BeginChangeCheck();
+		DrawGUI();
+		if (EditorGUI.EndChangeCheck()) serializedObject.ApplyModifiedProperties();	
+	}
+
+	private void HandleProp( SerializedProperty prop)
+	{
+		if( prop.name == "m_Script")
+        {
+			mSprictProp_ = prop.Copy();
+			return;
+        }
+
+		var info = GetFieldInfo( prop );
+		if( info == null ) return;
+
+		Type type = info.DeclaringType;
+
+		if( attributesArray_.ContainsKey( type ))
+        {
+			bool isSuccess = HandlePropSub(attributesArray_[type].categories, prop );
+			if( !isSuccess )
+            {
+				attributesArray_[type].props.Add(prop.Copy());
+			}
+		}
+        else
+        {
+			Debug.LogError("dont found this type = " + type.Name );
+        }
+	}
+
+	private bool HandlePropSub( Dictionary<string, Category> _categories, SerializedProperty _prop )
+    {
+		if(_categories == null ) return false;
+
+		foreach (var category in _categories)
+		{
+			if (category.Value.types.Contains(_prop.name))
+			{
+				category.Value.props.Add(_prop.Copy());
+				return true;
+			}
+            else
+            {
+				bool isSuccess = HandlePropSub(category.Value.categories, _prop );
+				if( isSuccess )
+                {
+					return true;
+                }
+			}
+		}
+		return false;
+	}
+
+	private void DrawGUI()
+    {
+		//没有序列化内容则不需要自定义渲染		
+		if (attributesArray_.Count == 0)
+		{
+			DrawDefaultInspector();
+			return;
+		}
+
+		if (!initialized_)
+		{
+			for (var i = 0; i < objectFields_.Count; i++)
+			{
+				Type currentType = objectFields_[i].DeclaringType;
+
+				var category = Attribute.GetCustomAttribute(objectFields_[i], typeof(CategoryAttribute)) as CategoryAttribute;				
+				if (category != null)
+				{
+					string[] categoryName = category.name_.Split('|');
+					Category c = null;
+					var cArray = attributesArray_[currentType].categories;
+					for (int j = 0; j < categoryName.Length; j++)
+					{
+						if(categoryName[j].Length == 0 ) continue;
+						if (!cArray.ContainsKey(categoryName[j]))
+						{
+							cArray.Add(categoryName[j], new Category{ expanded = category.isFoldout_ ,
+																	  useHorizontalLine = category.useHorizontalLine_ ,
+																	  titleName = category.titleName_ }); 
+						}
+						c = cArray[categoryName[j]];
+						cArray = c.categories;
+					}
+					if (c.types == null) c.types = new HashSet<string>();
+					c.types.Add(objectFields_[i].Name);
+				}                			
+			}
+
+			var property = serializedObject.GetIterator();
+			var next = property.NextVisible(true);
+			if (next)
+			{
+				do
+				{
+					HandleProp(property);
+				} while (property.NextVisible(false));
+			}
+		}
+
+		initialized_ = true;
+
+		using (new EditorGUI.DisabledScope( mSprictProp_ != null ))
+		{
+			EditorGUILayout.PropertyField(mSprictProp_ , true);
+		}
+
+		int count = 0;
+		foreach ( var attributes in attributesArray_ )
+        {
+			if (count > 0) { EditorGUILayout.Space(); }
+			EditorGUILayout.LabelField(attributes.Key.Name, scriptTagStyle_);
+
+			foreach (var prop in attributes.Value.props)
+			{
+				string tooltip = prop.tooltip != "" ? prop.name + "\n" + prop.tooltip : prop.name;
+				EditorGUILayout.PropertyField(prop, new GUIContent(prop.displayName, tooltip), true);
+			}
+
+			CategoryDraw(attributes.Value.categories);
+			count++;
+		}
+	}
+
+	private void CategoryDraw( Dictionary<string, Category> _categories, int _count = 0)
+    {
+		foreach( var category in _categories )
+        {
+			if (_count == 0)
+			{
+				//使用分割线
+				if (category.Value.useHorizontalLine)
+				{
+					EditorGUILayout.Space();
+					HorizontalLine.Create(0);
+					EditorGUILayout.Space();
+				}
+				//使用标签
+				if (category.Value.titleName != null)
+				{
+					EditorGUILayout.LabelField(category.Value.titleName, categorryTitleStyle_);
+				}
+			}
+
+			Rect rect;
+			{
+				rect = EditorGUILayout.BeginVertical();
+
+				EditorGUI.indentLevel = _count;
+				category.Value.expanded = EditorGUILayout.Foldout(category.Value.expanded, category.Key, EditorStyles.foldout);
+
+				EditorGUILayout.EndVertical();
+			}
+
+			{
+				rect = EditorGUILayout.BeginVertical();
+
+				if (category.Value.expanded)
+				{
+					for (int i = 0; i < category.Value.props.Count; i++)
+					{
+						EditorGUI.indentLevel = _count + 1;
+						string tooltip = category.Value.props[i].tooltip != "" ? category.Value.props[i].name + "\n" + category.Value.props[i].tooltip : category.Value.props[i].name;
+						EditorGUILayout.PropertyField(category.Value.props[i], new GUIContent(category.Value.props[i].displayName, tooltip), true);
+					}
+				}
+
+				EditorGUI.indentLevel = _count;
+				EditorGUILayout.EndVertical();
+			}
+
+			if (category.Value.categories != null)
+			{
+				if (category.Value.expanded)
+				{
+					CategoryDraw(category.Value.categories, _count + 1);
+				}
+			}
+		}
+	}
+
+	private class Category
+	{
+		public Dictionary<string, Category> categories = new Dictionary<string, Category>();
+		public List<SerializedProperty> props = new List<SerializedProperty>();
+		public HashSet<string> types = new HashSet<string>();
+		public bool   expanded;
+		public bool   useHorizontalLine;
+		public string titleName;
+
+		public void Clear()
+		{
+			categories.Clear();
+			props.Clear();
+			types.Clear();
+		}
+	}
+
+	private class Attributes
+	{
+		public Dictionary<string, Category> categories = new Dictionary<string, Category>();
+		public List<SerializedProperty> props = new List<SerializedProperty>();
+
+		public void AllClear()
+        {
+			AllClearSub(categories);
+			categories.Clear();
+			props.Clear();
+		}
+
+		private void AllClearSub(Dictionary<string, Category> _categories)
+        {
+			if(_categories == null ) return;
+			foreach( var category in _categories)
+            {
+				AllClearSub(category.Value.categories);
+				category.Value.Clear();
+			}
+        }
+	}
+
+	private bool IsTypeCompatible(Type _type)
+	{
+		if (_type == null || !(_type.IsSubclassOf(typeof(MonoBehaviour)) || _type.IsSubclassOf(typeof(ScriptableObject)))) return false;
+		return true;
+	}
+
+	private FieldInfo GetFieldInfo( SerializedProperty _property)
+	{
+		FieldInfo GetField(Type type, string path)
+		{
+			var info = type.GetField(path , BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.DeclaredOnly);
+			if( info == null && type.BaseType != null )
+            {
+				return GetField( type.BaseType , path );
+            }
+			return info;
+		}
+
+		var parentType = _property.serializedObject.targetObject.GetType();
+		var splits = _property.propertyPath.Split('.');
+		var fieldInfo = GetField(parentType, splits[0]);
+		for (var i = 1; i < splits.Length; i++)
+		{
+			if (splits[i] == "Array")
+			{
+				i += 2;
+				if (i >= splits.Length) continue;
+
+				var type = fieldInfo.FieldType.IsArray ? fieldInfo.FieldType.GetElementType() : fieldInfo.FieldType.GetGenericArguments()[0];
+				fieldInfo = GetField(type, splits[i]);
+			}
+			else
+			{
+				fieldInfo = i + 1 < splits.Length && splits[i + 1] == "Array" ? GetField(parentType, splits[i]) : GetField(fieldInfo.FieldType, splits[i]);
+			}
+
+			if (fieldInfo == null)
+			{
+				throw new Exception("Invalid FieldInfo. " + _property.propertyPath);
+			}
+			parentType = fieldInfo.FieldType;
+		}
+		return fieldInfo;
+	}
+}
---
+++ VariableAttributesExtend/Script/VariableAttributesExtend.cs
@@ -0,0 +1,434 @@
+/**
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2022 YuloongBY - Github: github.com/YuloongBY
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+using UnityEngine;
+
+#if UNITY_EDITOR
+using System;
+using UnityEditor;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text.RegularExpressions;
+#endif
+
+/// <summary>
+/// Category
+/// </summary>
+#if UNITY_EDITOR
+[AttributeUsage(AttributeTargets.Field)]
+#endif
+public class CategoryAttribute : PropertyAttribute
+{
+	public string name_              = null;
+    public bool   isFoldout_         = false;
+    public bool   useHorizontalLine_ = false;
+    public string titleName_         = null;
+
+    /// <summary>
+    /// Category 
+    /// </summary>
+    /// <param name="_name">Category name</param>
+    /// <param name="_isFoldout">Whether the initial state is foldout or not</param>
+    /// <param name="_useHorizontalLine">Add a split line above this category</param>   
+    /// <param name="_titleName">Add a title above this category ，"null" means not to use the title</param>
+    public CategoryAttribute(string _name, bool _isFoldout = false , bool _useHorizontalLine = false , string _titleName = null )
+	{
+		this.name_              = _name;
+        this.isFoldout_         = _isFoldout;
+        this.useHorizontalLine_ = _useHorizontalLine;
+        this.titleName_         = _titleName;
+    }
+}
+
+/// <summary>
+/// When you Rename or Hide an enumeration item, 
+/// you need to add attribute or [Rename] attribute above the enumeration variable
+/// </summary>
+#if UNITY_EDITOR
+[AttributeUsage(AttributeTargets.Field)]
+#endif
+public class OnlyEditEnumAttribute : PropertyAttribute
+{
+    /// <summary>
+    /// When you Rename or Hide an enumeration item, 
+    /// you need to add attribute or [Rename] attribute above the enumeration variable
+    /// </summary>
+    public OnlyEditEnumAttribute(){}
+}
+
+/// <summary>
+/// Hide this enumeration item
+/// </summary>
+#if UNITY_EDITOR
+[AttributeUsage(AttributeTargets.Field)]
+#endif
+public class HideEnumAttribute : PropertyAttribute
+{
+    /// <summary>
+    /// Hide this enumeration item
+    /// </summary>
+    public HideEnumAttribute(){}
+}
+
+/// <summary>
+/// Rename
+/// </summary>
+#if UNITY_EDITOR
+[AttributeUsage(AttributeTargets.Field)]
+#endif
+public class RenameAttribute : PropertyAttribute
+{
+    public string name_      = "";
+    public string htmlColor_ = "#000000";
+
+    /// <summary> Rename </summary>
+    /// <param name="_name">New name</param>
+    public RenameAttribute(string _name)
+    {
+        this.name_ = _name;
+#if UNITY_EDITOR
+        this.htmlColor_ = EditorGUIUtility.isProSkin ? "#D0D0D0" : "#000000";
+#else
+        this.htmlColor_ = "#000000";
+#endif
+    }
+
+    /// <summary> Rename </summary>
+    /// <param name="_name">New name</param>
+    /// <param name="_htmlColor">Text color (For example："#FFFFFF" or "black")</param>
+    public RenameAttribute(string _name, string _htmlColor)
+    {
+        this.name_      = _name;
+        this.htmlColor_ = _htmlColor;
+    }
+}
+
+/// <summary>
+/// Add a title above this variable
+/// </summary>
+#if UNITY_EDITOR
+[AttributeUsage(AttributeTargets.Field)]
+#endif
+public class TitleAttribute : PropertyAttribute
+{
+    public string title_             = "";
+    public string htmlColor_         = "#606060";
+    public bool   useHorizontalLine_ = false;
+
+    /// <summary>
+    /// Add a title above this variable
+    /// </summary>
+    /// <param name="_title">Title name</param>
+    /// <param name="_useHorizontalLine">Add a split line above this title</param>
+    public TitleAttribute(string _title , bool _useHorizontalLine = false )
+    {
+        this.title_ = _title;
+        this.useHorizontalLine_ = _useHorizontalLine;
+
+#if UNITY_EDITOR
+        this.htmlColor_ = EditorGUIUtility.isProSkin ? "#999999" : "#606060";
+#else
+        this.htmlColor_ = "#606060";
+#endif
+    }
+
+    /// <summary>
+    /// Add a title above this variable
+    /// </summary>
+    /// <param name="_title">Title name</param>
+    /// <param name="_htmlColor">Text color (For example："#FFFFFF" or "black")</param>
+    /// <param name="_useHorizontalLine">Add a split line above this title</param>
+    public TitleAttribute(string _title, string _htmlColor, bool _useHorizontalLine = false )
+    {
+        this.title_ = _title;
+        this.htmlColor_ = _htmlColor;
+        this.useHorizontalLine_ = _useHorizontalLine;
+    }
+}
+
+/// <summary>
+/// Add a split line above this variable
+/// </summary>
+#if UNITY_EDITOR
+[AttributeUsage(AttributeTargets.Field)]
+#endif
+public class HorizontalLineAttribute: PropertyAttribute
+{
+    /// <summary>
+    /// Add a split line above this variable
+    /// </summary>
+    public HorizontalLineAttribute(){}
+}
+
+#if UNITY_EDITOR
+[CustomPropertyDrawer(typeof(RenameAttribute))]
+[CustomPropertyDrawer(typeof(OnlyEditEnumAttribute))]
+public class RenameDrawer : PropertyDrawer
+{
+    public override void OnGUI(Rect _position, SerializedProperty _property, GUIContent _label)
+    {
+        //保存当前颜色
+        Color defaultColor = EditorStyles.label.normal.textColor;
+
+        // 替换属性名称        
+        if ( attribute is RenameAttribute )
+        {
+            //重命名
+            RenameAttribute rename = (RenameAttribute)attribute;
+            _label.text = rename.name_;
+            
+            //重绘GUI
+            EditorStyles.label.normal.textColor = HtmlToColor.Change(rename.htmlColor_);
+            bool isElement = Regex.IsMatch(_property.displayName, "Element \\d+");
+            if (isElement) _label.text = _property.displayName;
+        }
+
+        //枚举
+        if (_property.propertyType == SerializedPropertyType.Enum)
+        {
+            //枚举内的重命名和隐藏是否生效
+            bool isEnumEdit = attribute is OnlyEditEnumAttribute || attribute is RenameAttribute;
+            DrawEnum(_position, _property, _label , isEnumEdit );
+        }
+        //一般
+        else
+        { 
+            EditorGUI.PropertyField(_position, _property, _label, true);                            
+        }
+
+        //颜色重置
+        EditorStyles.label.normal.textColor = defaultColor;
+    }
+
+    /// <summary>
+    /// 绘制枚举类型
+    /// </summary>
+    private void DrawEnum(Rect _position, SerializedProperty _property, GUIContent _label , bool _isEditEnum )
+    {
+        EditorGUI.BeginChangeCheck();
+
+        // 获取枚举相关属性
+        Type type = fieldInfo.FieldType;
+        string[] names = _property.enumNames;
+        string[] values = new string[names.Length];
+        Array.Copy(names, values, names.Length);
+        while (type.IsArray) type = type.GetElementType();
+
+        //枚举可编辑
+        if( _isEditEnum )
+        {
+            for (int i = 0; i < names.Length; i++)
+            {
+                FieldInfo info = type.GetField(names[i]);
+
+                //枚举内容是否被隐藏
+                HideEnumAttribute hideAtr = info.GetCustomAttribute(typeof(HideEnumAttribute), true) as HideEnumAttribute;
+                if (hideAtr != null)
+                {
+                    //被隐藏则将其设置为空
+                    values[i] = null;
+                }
+                else
+                {
+                    //没有被隐藏则重命名
+                    RenameAttribute[] renameAtts = (RenameAttribute[])info.GetCustomAttributes(typeof(RenameAttribute), true);
+                    if (renameAtts.Length != 0) values[i] = renameAtts[0].name_;
+                }
+            }
+        }
+
+        //创造显示枚举列表
+        List<string> drawEnum = new List<string>( values.Length );
+        List<GUIContent> guiContentArray = new List<GUIContent>(values.Length);
+        for ( int nCnt = 0 ; nCnt < values.Length ; nCnt ++ )
+        {
+            if( values[ nCnt ]!= null)
+            { 
+                drawEnum.Add( values[ nCnt ]);
+                guiContentArray.Add(new GUIContent(values[nCnt]));
+            }            
+        }
+        
+        //显示枚举索引初期设置
+        if( drawEnumIdx < 0 )
+        {
+            //如果该索引的枚举可以被显示则带入到显示枚举索引，否则为0
+            string value = values[ _property.enumValueIndex ];
+            drawEnumIdx = value == null ? 0 : drawEnum.IndexOf(value);
+        }
+
+        // 重绘GUI
+        //drawEnumIdx = EditorGUI.Popup(position, label.text, drawEnumIdx, drawEnum.ToArray());
+        drawEnumIdx = EditorGUI.Popup(_position, _label, drawEnumIdx, guiContentArray.ToArray());
+
+        //获取选中枚举内容
+        string selectedEnum = drawEnum[ drawEnumIdx ];
+        
+        //获取该枚举内容在实际枚举中的索引
+        int index = Array.IndexOf( values , selectedEnum );
+        //设置枚举
+        if (EditorGUI.EndChangeCheck() && index != -1) _property.enumValueIndex = index;
+    }
+    //显示用枚举的索引
+    private int drawEnumIdx = -1;
+
+    public override float GetPropertyHeight(SerializedProperty _property, GUIContent _label)
+    {
+        return EditorGUI.GetPropertyHeight(_property, _label, true);
+    }
+}
+#endif
+
+#if UNITY_EDITOR
+[CustomPropertyDrawer(typeof(TitleAttribute))]
+public class TitleAttributeDrawer : DecoratorDrawer
+{
+    // 文本样式
+    private GUIStyle style = new GUIStyle(EditorStyles.label);
+
+    public override void OnGUI(Rect _position)
+    {
+        // 获取Attribute
+        TitleAttribute title = (TitleAttribute)attribute;
+
+        if( title.useHorizontalLine_ )
+        {
+            HorizontalLine.Create( -45 );
+        }
+
+        style.fixedHeight = title.useHorizontalLine_ ? 50 : 25;
+        style.normal.textColor = HtmlToColor.Change(title.htmlColor_);
+        style.fontSize = 13;
+        // 重绘GUI
+        _position = EditorGUI.IndentedRect(_position);
+        GUI.Label(_position, title.title_, style);
+    }
+
+    public override float GetHeight()
+    {
+        TitleAttribute title = (TitleAttribute)attribute;        
+        return base.GetHeight() + ( title.useHorizontalLine_ ? 15 : 5 );
+    }
+}
+#endif
+
+#if UNITY_EDITOR
+[CustomPropertyDrawer(typeof(HorizontalLineAttribute))]
+public class HorizontalLineDrawer : DecoratorDrawer
+{
+    public override void OnGUI(Rect _position)
+    {
+        HorizontalLine.Create( -30 );
+    }
+
+    public override float GetHeight()
+    {
+        return base.GetHeight();
+    }
+}
+#endif
+
+#if UNITY_EDITOR
+public static class HorizontalLine
+{
+    public static void Create( float _yOffset )
+    {
+        var rect = EditorGUILayout.BeginVertical();
+        rect.height = 1;
+        rect.y += _yOffset;
+        Color color = EditorGUIUtility.isProSkin ? new Color(0.1f, 0.1f, 0.1f, 1.0f ) : new Color(0.5f, 0.5f, 0.5f, 1);
+        EditorGUI.DrawRect(rect, color );
+        EditorGUILayout.EndVertical();
+    }
+}
+#endif
+
+#if UNITY_EDITOR
+public static class HtmlToColor
+{
+    /// <summary>
+    /// Html颜色转换为Color
+    /// </summary>
+    public static Color Change(string _hex)
+    {
+        // 编辑器默认颜色
+        if (string.IsNullOrEmpty(_hex)) return new Color(0.705f, 0.705f, 0.705f);
+
+        // 转换颜色
+        _hex = _hex.ToLower();
+        if (_hex.IndexOf("#") == 0 && _hex.Length == 7)
+        {
+            int r = Convert.ToInt32(_hex.Substring(1, 2), 16);
+            int g = Convert.ToInt32(_hex.Substring(3, 2), 16);
+            int b = Convert.ToInt32(_hex.Substring(5, 2), 16);
+            return new Color(r / 255f, g / 255f, b / 255f);
+        }
+        else if (_hex == "red")
+        {
+            return Color.red;
+        }
+        else if (_hex == "green")
+        {
+            return Color.green;
+        }
+        else if (_hex == "blue")
+        {
+            return Color.blue;
+        }
+        else if (_hex == "yellow")
+        {
+            return Color.yellow;
+        }
+        else if (_hex == "black")
+        {
+            return Color.black;
+        }
+        else if (_hex == "white")
+        {
+            return Color.white;
+        }
+        else if (_hex == "cyan")
+        {
+            return Color.cyan;
+        }
+        else if (_hex == "gray")
+        {
+            return Color.gray;
+        }
+        else if (_hex == "grey")
+        {
+            return Color.grey;
+        }
+        else if (_hex == "magenta")
+        {
+            return Color.magenta;
+        }
+        else if (_hex == "orange")
+        {
+            return new Color(1, 165 / 255f, 0);
+        }
+        return new Color(0.705f, 0.705f, 0.705f);
+    }
+}
+#endif
+
